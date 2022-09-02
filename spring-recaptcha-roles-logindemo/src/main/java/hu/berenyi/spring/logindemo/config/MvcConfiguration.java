9a4891109bd44d91be617452e53d344834c7b62c
Lajos Berenyi <lajos2.berenyi@raiffeisen.hu>
2022-09-02 09:13:11 +0200

    Avoiding the use of deprecated WebSecurityConfigurerAdapter in MvcConfiguration

--- spring-recaptcha-roles-logindemo/src/main/java/hu/berenyi/spring/logindemo/config/MvcConfiguration.java
+++ spring-recaptcha-roles-logindemo/src/main/java/hu/berenyi/spring/logindemo/config/MvcConfiguration.java
@@ -4,20 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.github.mkopylec.recaptcha.security.login.FormLoginConfigurerEnhancer;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @ComponentScan({"com.github.mkopylec.recaptcha", "hu.berenyi.spring.logindemo"})
-public class MvcConfiguration extends WebSecurityConfigurerAdapter {
+public class MvcConfiguration {
 	
 	@Autowired
 	private FormLoginConfigurerEnhancer enhancer;
@@ -36,23 +39,26 @@ public class MvcConfiguration extends WebSecurityConfigurerAdapter {
 		
 		return authProvider;
 	}
+	
+	@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {	
+		return authenticationConfiguration.getAuthenticationManager();
+    }
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		enhancer.addRecaptchaSupport(http.formLogin()).loginPage("/login")
+        .and()
+        .csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/login**").permitAll()
+        .antMatchers("/admin/**").hasRole("Administrator")
+        .antMatchers("/content/**").hasAnyRole("Administrator", "Content editor")
+        .antMatchers("/signedin/**").hasAnyRole("Administrator", "Signed in user")
+		.antMatchers("/**").authenticated()
+		.and().exceptionHandling().accessDeniedPage("/accessdenied");
+		http.authenticationProvider(authenticationProvider());
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(authenticationProvider());
-	}
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        enhancer.addRecaptchaSupport(http.formLogin()).loginPage("/login")
-                .and()
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/login**").permitAll()
-                .antMatchers("/admin/**").hasRole("Administrator")
-                .antMatchers("/content/**").hasAnyRole("Administrator", "Content editor")
-                .antMatchers("/signedin/**").hasAnyRole("Administrator", "Signed in user")
-        		.antMatchers("/**").authenticated()
-        		.and().exceptionHandling().accessDeniedPage("/accessdenied");
+        return http.build();
     }
 }
